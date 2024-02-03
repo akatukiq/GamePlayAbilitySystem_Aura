3faefaa55c7678bf7f3bd3d2c4f152717f1d2bf7
akatukiq <akatukiq@gmail.com>
2024-02-03 19:13:29 +0900

    Modifier Magnitude Calculation

---
+++ Source/Aura/Private/AbilitySystem/MMC/MMC_MaxMana.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/MMC/MMC_MaxMana.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "interaction/CombatInterface.h"
+
+UMMC_MaxMana::UMMC_MaxMana()
+{
+	IntDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();
+	IntDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	IntDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(IntDef);
+}
+
+float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	// Geter tags from source and target
+	const FGameplayTagContainer* SorceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SorceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float Int = 0.f;
+	GetCapturedAttributeMagnitude(IntDef, Spec, EvaluationParameters, Int);
+	Int = FMath::Max<float>(Int, 0.f);
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	return 50.f + 2.5f * Int + 15.f * PlayerLevel;
+
+}
