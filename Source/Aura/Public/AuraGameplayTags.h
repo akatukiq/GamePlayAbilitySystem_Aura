d650a914bf7e44aae3ffb44592dde45023978b2e
akatukiq <akatukiq@gmail.com>
2024-02-04 01:43:29 +0900

    Aura Asset Manager

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -16,6 +16,8 @@ public:
 	static const FAuraGameplayTags& Get() { return GameplayTags;}
 	static void InitializeNativeGameplayTags();
 
+	FGameplayTag Attributes_Secondary_Armor;
+
 protected:
 
 private:
