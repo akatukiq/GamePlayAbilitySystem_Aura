332fab9c7b69a71a4d125a4907d71682e1d995c2
akatukiq <akatukiq@gmail.com>
2024-02-19 19:51:00 +0900

    default Attribute Effect

---
+++ Source/Aura/Public/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+class UGameplayEffect;
+
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	Elementalist,
+	Warrior,
+	Ranger
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, Category = "Class Default")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Character Common Class Default")
+	TMap<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassInformation;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Default")
+	TSubclassOf<UGameplayEffect> SecondaryAttribute;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Common Class Default")
+	TSubclassOf<UGameplayEffect> VitalAttribute;
+
+	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass);
+
+};
