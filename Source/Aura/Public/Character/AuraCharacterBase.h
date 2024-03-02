e7ac3d92ee17b411b755706e3ea517c45ad4ab16
akatukiq <akatukiq@gmail.com>
2024-03-03 03:17:42 +0900

    Get Live Players With in Radius

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -25,8 +25,13 @@ public:
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 
+	/* Combat Interface */
 	virtual UAnimMontage* GetHitReactMontage_Implementation() override;
 	virtual void Die() override;
+	virtual FVector GetCombatSocketLocation_Implementation() override;
+	virtual bool IsDead_Implementation() const override;
+	virtual AActor* GetAvatar_Implementation() override;
+	/* End Combat Interface */
 
 	UFUNCTION(NetMulticast, Reliable)
 	virtual void MulticastHandleDeath();
@@ -40,7 +45,7 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Conbat")
 	FName WeaponTipSocketName;
 
-	virtual FVector GetCombatSocketLocation_Implementation() override;
+	bool bDead = false;
 
 	UPROPERTY()
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
@@ -78,7 +83,7 @@ protected:
 	TObjectPtr<UMaterialInstance> DissoleveMaterialInstance;
 
 	UPROPERTY(EditAnyWhere, BlueprintReadOnly)
-		TObjectPtr<UMaterialInstance> WeaponDissoleveMaterialInstance;
+	TObjectPtr<UMaterialInstance> WeaponDissoleveMaterialInstance;
 
 private:
 
