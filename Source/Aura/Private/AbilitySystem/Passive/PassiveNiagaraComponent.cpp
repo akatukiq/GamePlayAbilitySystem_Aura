0bac1fc7186c61d382a827a1b873cb35b2e88fb1
akatukiq <akatukiq@gmail.com>
2024-05-17 04:28:53 +0900

    Passive Niagara Component_Temp

---
+++ Source/Aura/Private/AbilitySystem/Passive/PassiveNiagaraComponent.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Passive/PassiveNiagaraComponent.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Character/AuraCharacterBase.h"
+
+UPassiveNiagaraComponent::UPassiveNiagaraComponent()
+{
+}
+
+void UPassiveNiagaraComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (UAuraAbilitySystemComponent* AuraASC = Cast<UAuraAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetOwner())))
+	{
+		AuraASC->ActivatePassiveEffect.AddUObject(this, &UPassiveNiagaraComponent::OnPassiveActivate);
+	}
+	else if (ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetOwner()))
+	{
+		CombatInterface->GetOnASCRegisteredDelegate().AddLambda([this](UAbilitySystemComponent* ASC)
+		{
+			if (UAuraAbilitySystemComponent* AuraASC = Cast<UAuraAbilitySystemComponent>(UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetOwner())))
+			{
+				AuraASC->ActivatePassiveEffect.AddUObject(this, &UPassiveNiagaraComponent::OnPassiveActivate);
+			}
+		});
+	}
+}
+
+void UPassiveNiagaraComponent::OnPassiveActivate(const FGameplayTag& AbilityTag, bool bActivate)
+{
+	if (AbilityTag.MatchesTagExact(PassiveSpellTag))
+	{
+		if (bActivate && !IsActive())
+		{
+			Activate();
+		}
+		else
+		{
+			Deactivate();
+		}
+	}
+}
