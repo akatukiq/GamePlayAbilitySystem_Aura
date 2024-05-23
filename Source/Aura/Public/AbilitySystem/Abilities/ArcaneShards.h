bdbc490e221deaaf8b7c3eb4ee724d55963d6e72
akatukiq <akatukiq@gmail.com>
2024-05-24 04:24:00 +0900

    Spell Descriptions

---
+++ Source/Aura/Public/AbilitySystem/Abilities/ArcaneShards.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "ArcaneShards.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UArcaneShards : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
