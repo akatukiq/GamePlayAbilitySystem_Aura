88d90eaccc241cad7c027953b949cee80c95c4e4
akatukiq <akatukiq@gmail.com>
2024-06-20 13:28:56 +0900

    Heghlighting Non-Enemies

--- Source/Aura/Public/CheckPoint/CheckPoint.h
+++ Source/Aura/Public/CheckPoint/CheckPoint.h
@@ -38,10 +38,9 @@ protected:
 
 	void HandleGlowEffects();
 
-private:
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TObjectPtr<UStaticMeshComponent> CheckpointMesh;
-
+private:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USphereComponent> Sphere;
 };
