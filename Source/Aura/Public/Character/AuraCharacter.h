732153b7c0cdc3e8e387b5131b53d76c181d0f46
akatukiq <akatukiq@gmail.com>
2024-06-24 23:44:41 +0900

    Player Death

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -44,8 +44,14 @@ public:
 	/* combat interface */
 
 	virtual int32 GetPlayerLevel_Implementation() override;
+	virtual void Die(const FVector& DeathImpulse) override;
 
 	/* end combat interface */
+
+	UPROPERTY(EditDefaultsOnly)
+	float DeathTime = 5.f;
+
+	FTimerHandle DeathTimer;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	TObjectPtr<UNiagaraComponent> LevelUpNiagaraComponent;
